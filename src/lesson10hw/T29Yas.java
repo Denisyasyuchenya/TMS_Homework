7c12e0cd95c15c95cae4ba70e6e1c07d10b2cea9
Denisyasyuchenya <yasyuchenya1@gmail.com>
2020-04-09 12:42:41 +0300

    homework for lesson 10

---
+++ src/lesson10hw/T29Yas.java
@@ -0,0 +1,26 @@
+package lesson10hw;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class T29Yas {
+
+    public static void main(String[] agrs){
+
+    /*Создать список оценок учеников с помощью ArrayList, заполнить случайными
+    оценками. Удалить неудовлетворительные оценки из списка.*/
+
+        ArrayList<Integer> list= new ArrayList<Integer>(10);
+        for(int i=0;i<10;i++){
+            Integer mark=(int) (Math.random()*10+1);
+            list.add(i,mark);
+        }
+        System.out.println(list);
+        for(Iterator<Integer> it = list.iterator(); it.hasNext();){
+            if(it.next()<4)
+                it.remove();
+        }
+        System.out.println(list);
+
+    }
+}
